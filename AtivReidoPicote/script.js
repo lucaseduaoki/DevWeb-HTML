884f181d489afcb8c4f96ba9cddcec4b616f13be
Lucas Aoki <161040368+lucaseduaoki@users.noreply.github.com>
2024-12-13 15:47:51 -0300

    Update script.js

--- AtivReidoPicote/script.js
+++ AtivReidoPicote/script.js
@@ -1,16 +1,26 @@
-//Tela Splash
 document.addEventListener("DOMContentLoaded", function() {
     const splashScreen = document.querySelector('.splash-screen');
     const mainContent = document.querySelector('.container');
-    
+    const loginForm = document.querySelector('.login-form'); // Seleciona o formulário de login
+
+    // Função que esconde a tela Splash após 3 segundos e exibe o conteúdo principal
     function hideSplashScreen() {
         splashScreen.style.opacity = 0;
+        splashScreen.style.visibility = "hidden"; // Esconde a tela splash após o fade
         setTimeout(() => {
-            splashScreen.style.display = 'none';
-            mainContent.style.display = 'block';
-        }, 5000);
+            splashScreen.style.display = 'none'; // Remove do fluxo
+            mainContent.style.display = 'block'; // Exibe a tela de login
+        }, 1000); // Tempo após a animação de fade
     }
 
-    setTimeout(hideSplashScreen, 3000); 
-});
+    // Esconde a tela Splash após 3 segundos
+    setTimeout(hideSplashScreen, 3000);
 
+    // Adicionando evento de submit no formulário de login
+    loginForm.addEventListener('submit', function(event) {
+        event.preventDefault(); // Impede o envio padrão do formulário (evita o reload da página)
+        
+        // Redirecionar diretamente para a página principal após o login
+        window.location.href = "home.html"; // Alterar para a página desejada (página principal)
+    });
+});
